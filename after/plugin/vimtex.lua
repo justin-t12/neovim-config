1b00078014dea3b948f6bb723919db204d028a71
justin-t12 <justin.t12@outlook.com>
2023-11-20 12:43:56 -0500

    allowed latexmk to use shell escaping for minted latex package

--- after/plugin/vimtex.lua
+++ after/plugin/vimtex.lua
@@ -4,6 +4,15 @@ function vimtexInit()
     vim.g.maplocalleader = ","
     vim.g.tex_flavor='latex'
     vim.g.vimtex_quickfix_mode = 0
+    vim.g.vimtex_compiler_latexmk = {
+      options = {
+        '-shell-escape',
+        '-verbose',
+        '-file-line-error',
+        '-synctex=1',
+        '-interaction=nonstopmode'
+      }
+    }
 end
 
 vimtexInit()
