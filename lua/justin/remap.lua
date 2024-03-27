63954dfcc60be761cf38ac3705b9e772b520ae8e
justin-t12 <justin.t12@outlook.com>
2024-03-27 13:18:47 -0400

    fixed tabs keymaps

--- lua/justin/remap.lua
+++ lua/justin/remap.lua
@@ -21,5 +21,5 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 vim.keymap.set("n", "<leader><TAB>n", ":tabnew<CR>")
 vim.keymap.set("n", "<leader><TAB>d", ":tabclose<CR>")
-vim.keymap.set("n", "<leader><TAB>j", ":tabnext<CR>")
-vim.keymap.set("n", "<leader><TAB>k", ":tabprevious<CR>")
+vim.keymap.set("n", "<leader><TAB>k", ":tabnext<CR>")
+vim.keymap.set("n", "<leader><TAB>j", ":tabprevious<CR>")
